4f90aaaf2c856147c70d7889632ee9111c1499d3
T0b1111000 <killiandangta@gmail.com>
2026-01-15 10:25:43 +0100

    Update DemoGithubApplication.java

--- src/test/java/com/example/demo_github/DemoGithubApplication.java
+++ src/test/java/com/example/demo_github/DemoGithubApplication.java
@@ -6,14 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class DemoGithubApplication {  // ← ICI et dans main()
+public class DemoGithubApplication{
     public static void main(String[] args) {
-        SpringApplication.run(DemoGithubApplication.class, args);  // ← ICI
+        SpringApplication.run(DemogithubApplication.class, args);
     }
+}
 
-    @GetMapping("/hello")
-    public String hello() {
+@RestController
+class HelloWorldController
+    @GetMapping("/")
+    public String sayHello() {
         return "Hello, World!";
     }
 }
