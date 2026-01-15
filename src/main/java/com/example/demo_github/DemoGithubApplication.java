f49e9f5b2417b21063655990be05a7cbffef9d2a
T0b1111000 <killiandangta@gmail.com>
2026-01-15 10:30:32 +0100

    Update DemoGithubApplication.java

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -1,19 +1,23 @@
-package com.example.dockerdemo;  // Ton package
-
+package com.example.dockerdemo;
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 @SpringBootApplication
-@RestController  // Ajoute ça
-public class DockerDemoApplication {  // Ou ton nom de classe
+public class DockerDemoApplication {
+ 
     public static void main(String[] args) {
         SpringApplication.run(DockerDemoApplication.class, args);
     }
-
-    @GetMapping("/hello")  // Ajoute cette méthode
-    public String hello() {
-        return "Hello, World!";
+ 
+}
+ 
+@RestController
+class HelloWorldController {
+    @GetMapping("/")
+    public String seyHello() {
+        return "Hello, World !";
     }
 }
