95183cd73bb1f7ac75869f6b98294e11f3956ae4
T0b1111000 <killiandangta@gmail.com>
2026-01-15 10:27:33 +0100

    Update DemoGithubApplication.java

--- src/test/java/com/example/demo_github/DemoGithubApplication.java
+++ src/test/java/com/example/demo_github/DemoGithubApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo_github;  // Adapte à ton dossier
+package com.example;  // Adapte à ton dossier
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
